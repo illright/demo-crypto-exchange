5bed6cbe9cfb29ef7cd2ad51f0a502fb24db2ad9
Lev Chelyadinov <leva181777@gmail.com>
2022-05-24 23:27:14 +0300

    Implement the logic of trading on the backend

--- src/app/models/order.server.ts
+++ src/app/models/order.server.ts
@@ -1,4 +1,5 @@
 import type { Order } from "@prisma/client";
+import { Decimal } from "@prisma/client/runtime";
 
 import { prisma } from "~/app/db.server";
 
@@ -14,6 +15,36 @@ export function listOrders({
   });
 }
 
-export function createOrder(order: Omit<Order, 'id'>) {
-  return prisma.order.create({ data: order });
+export async function createOrder(order: Omit<Order, "id">) {
+  const oppositeType = order.type === "buy" ? "sell" : "buy";
+  const orders = await prisma.order.findMany({
+    where: {
+      currencyFromId: order.currencyFromId,
+      currencyToId: order.currencyToId,
+      type: oppositeType,
+      price: { lte: order.price },
+    },
+    select: { id: true, amount: true },
+    orderBy: { price: "asc" },
+  });
+
+  let left = order.amount.toNumber();
+  for (const otherOrder of orders) {
+    if (left >= otherOrder.amount.toNumber()) {
+      left -= otherOrder.amount.toNumber();
+      await prisma.order.delete({ where: { id: otherOrder.id } });
+    } else {
+      await prisma.order.update({
+        where: { id: otherOrder.id },
+        data: { amount: otherOrder.amount.sub(left) },
+      });
+      left = 0;
+      break;
+    }
+  }
+
+  if (left !== 0) {
+    order.amount = new Decimal(left);
+    return prisma.order.create({ data: order });
+  }
 }
