296d16c3c51b948fdb3147cf10c486e4d4cd250d
Lev Chelyadinov <leva181777@gmail.com>
2022-05-30 21:44:08 +0300

    Add the order type tabs

--- app/routes/__trade.tsx
+++ app/routes/__trade.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { Outlet, useLoaderData } from "@remix-run/react";
 import { type LoaderFunction } from "@remix-run/node";
 
 import { CurrencyPickers } from "~/features/select-currencies";
+import { OrderTypeTabs } from "~/features/select-order-type";
 import { listCurrencies, type Currency } from "~/entities/currency";
+import type { OrderType } from "~/entities/order";
 
 export const loader: LoaderFunction = async () => {
   return await listCurrencies();
@@ -10,10 +13,18 @@ export const loader: LoaderFunction = async () => {
 
 export default function TradePage() {
   const currencies = useLoaderData<Currency[]>();
+  const [orderType, setOrderType] = useState<OrderType>("buy");
 
   return (
     <>
-      <CurrencyPickers options={currencies} />
+      <div className="p-2">
+        <OrderTypeTabs value={orderType} onChange={setOrderType}>
+          {[
+            <CurrencyPickers key="buy" orderType="buy" options={currencies} />,
+            <CurrencyPickers key="sell" orderType="sell" options={currencies} />,
+          ]}
+        </OrderTypeTabs>
+      </div>
       <Outlet />
     </>
   );
